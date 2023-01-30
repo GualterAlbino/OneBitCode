edc9e56d8231a9490ba8367735987b1faf63d342
Gualter Albino <gualteralbino1000@gmail.com>
2023-01-30 20:52:53 -0300

    Estudo do dia 30/01/2023

--- JavaScript II - DOM/Aula12 - Manipulando Atributos com JavaScript/index.js
+++ JavaScript II - DOM/Aula12 - Manipulando Atributos com JavaScript/index.js
@@ -1,5 +1,47 @@
 const input = document.getElementById('input');
 
 document.getElementById('value').addEventListener('click', function(){
-    input.value = 'Olá, mundo!';
+    //input.value = 'Olá, mundo!';
+    console.log(input.value);
+
+    //input.value = input.value === '' ? 'Olá, mundo' : '';
+    if(input.value === ''){
+        input.value = 'Olá,mundo!';
+    }
+
+    console.log(input.value); //mostra o valor em tempo real
+    console.log(input.getAttribute('value'))//mostra o valor pre-definido no HTML
+
+    
+});
+
+//trocar de input para radio e vice versa
+document.getElementById('type').addEventListener('click',function(){
+    //input.type = input.type !== 'radio' ? 'radio' : 'text'
+
+    if(input.type !== 'radio'){
+        input.type = 'radio';
+    }else{
+        input.type = 'text';
+    }
+
+    //input.setAttribute('type','radio');
+});
+
+//Colocar o texto de fundo
+document.getElementById('placeholder').addEventListener('click',function () {
+    input.placeholder = 'Digite algo....';
+});
+
+//Desabilitar o input
+document.getElementById('disable').addEventListener('click',function(){
+    input.setAttribute('disabled', !input.disabled);
+    //disabled é um atributo booleano
+});
+
+document.getElementById('data').addEventListener('click',function(){
+    const data = input.dataset.somethingElse
+    console.log("O valor do atributo data-something-else é:" + data);
+    input.dataset.somethingElse = 'Algum outro valor'
+    console.log("O valor do atributo data-something-else agora é: " + input.dataset.somethingElse);
 });
