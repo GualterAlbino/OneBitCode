260832397a698cfab8cecd45ff2a92d704872152
Gualter Albino <gualteralbino1000@gmail.com>
2023-03-06 23:25:08 -0300

    Estudo do dia 06/03/2023

---
+++ OneBitCode/JavaScript III - JS Moderno/Aula09 - Encadeamento Opcional/index.js
@@ -0,0 +1,39 @@
+/*
+
+
+Operador que permite ler propriedades internas de uma cadeia de objetos, mas com o diferencial
+de que ELE NÃO FAZ A VALIDAÇÃO, usando o operador "?"
+
+Em vez de usar o "." , ele usa o "?"
+
+
+*/
+
+const user = {
+    name: "John Doe",
+    email: "doejohn@email.com",
+    friends: [{
+      name: "Mary",
+      address: {
+        street: "Some Street",
+        number: 89
+      }
+    }],
+    age: 42,
+    phone: {
+      countryCode: "+55",
+      ddd: "22",
+      number: "998765432"
+    }
+}
+
+//Nesse caso, não existe o phone em FRIENDS
+//console.log(user.friends[0].phone.ddd);
+
+//Para usar o encadeamento opcional (Caso exista)
+console.log(user?.friends[0]?.phone?.ddd)
+console.log(user?.brothers?.length)
+
+console.log(user?.name)
+
+//Verifica e caso der erro retorna UNDEFINED
